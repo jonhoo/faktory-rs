3b9171543ffee5fc5d398a39de2abce0411ba90f
Pavel Mikhalkevich <paul.michelle.prod@gmail.com>
2024-01-08 15:32:12 +0500

    Resolce check warnings in error module

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,10 @@
 //! Enumerates all errors that this crate may return.
 //!
-//! [`Error`] is the top level error enum.
+//! [`enum@Error`] is the top level error enum.
 //! Most consumers should only need to interact with this type.
 //! This is also where more generic errors such as I/O errors are placed,
-//! whereas the more specific errors ([`Connection`] and [`Protocol`]) are
-//! related to logic.
+//! whereas the more specific errors ([`Connect`] and [`Protocol`])
+//! are related to logic.
 //!
 //! [`Connect`] describes errors specific to the connection logic, for example
 //! version mismatches or an invalid URL.
