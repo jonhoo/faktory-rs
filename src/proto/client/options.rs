a1a701efdd48d5278e2a5530069dc3694a853e57
--show-origin <paul.michelle.prod@gmail.com>
2024-04-04 22:56:49 +0500

    Derive Debug for ClientOptions

--- src/proto/client/options.rs
+++ src/proto/client/options.rs
@@ -1,29 +1,23 @@
 use crate::proto::WorkerId;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub(crate) struct ClientOptions {
-    /// Hostname to advertise to server.
-    /// Defaults to machine hostname.
+    // Hostname to advertise to server. Defaults to machine hostname.
     pub(crate) hostname: Option<String>,
 
-    /// PID to advertise to server.
-    /// Defaults to process ID.
+    // PID to advertise to server. Defaults to process ID.
     pub(crate) pid: Option<usize>,
 
-    /// Worker ID to advertise to server.
-    /// Defaults to a GUID.
+    // Worker ID to advertise to server Defaults to a GUID.
     pub(crate) wid: Option<WorkerId>,
 
-    /// Labels to advertise to se/// A stream that can be re-established after failing.rver.
-    /// Defaults to ["rust"].
+    // Labels to advertise to server. Defaults to ["rust"].
     pub(crate) labels: Vec<String>,
 
-    /// Password to authenticate with
-    /// Defaults to None.
+    // Password to authenticate with. Defaults to None.
     pub(crate) password: Option<String>,
 
-    /// Whether this client is instatianted for
-    /// a worker (i.e. to consume jobs).
+    // Whether this client is instatianted for a worker (i.e. to consume jobs).
     pub(crate) is_worker: bool,
 }
 
