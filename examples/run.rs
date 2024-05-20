aaf79d1d7a92f193d9c4adab15cf2dd9fdf188fb
--show-origin <paul.michelle.prod@gmail.com>
2024-05-20 16:25:27 +0400

    Add tracing and examples for `run` and `run_to_completion`

---
+++ examples/run.rs
@@ -0,0 +1,87 @@
+use async_trait::async_trait;
+use faktory::{Client, Job, JobRunner, Worker};
+use std::io::{Error as IOError, Result as IOResult};
+use std::sync::Arc;
+use tokio::sync::mpsc;
+use tokio::time;
+
+pub struct JobHandler<T> {
+    chan: Arc<mpsc::Sender<T>>,
+}
+
+impl<T> JobHandler<T> {
+    pub fn new(chan: &Arc<mpsc::Sender<T>>) -> Self {
+        Self {
+            chan: Arc::clone(chan),
+        }
+    }
+}
+
+impl JobHandler<u64> {
+    async fn process_one(&self, job: Job) -> IOResult<()> {
+        time::sleep(time::Duration::from_millis(100)).await;
+        let mut args = job.args().into_iter();
+        let x = args
+            .next()
+            .expect("'x' to be some")
+            .as_u64()
+            .expect("'x' to be an integer");
+        let y = args
+            .next()
+            .expect("'y' to be some")
+            .as_u64()
+            .expect("'y' to be an interger");
+        self.chan.send(x + y).await.expect("no error");
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl JobRunner for JobHandler<u64> {
+    type Error = IOError;
+
+    async fn run(&self, job: Job) -> Result<(), Self::Error> {
+        self.process_one(job).await.unwrap();
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .init();
+
+    // create a sender and a receiver; the former will be passed
+    // to the job handler and will be used to send the processing result
+    // down to the receiving end;
+    let (tx, mut rx) = mpsc::channel(100);
+    let tx = Arc::new(tx);
+
+    // create a producing client
+    let mut c = Client::connect(None)
+        .await
+        .expect("client successfully connected");
+
+    // push one job onto the default queue
+    c.enqueue(Job::builder("job_type").args(vec![5, 8]).build())
+        .await
+        .expect("enqueued ok");
+
+    // create a worker
+    let mut w = Worker::builder()
+        .register("job_type", JobHandler::new(&tx))
+        .connect(None)
+        .await
+        .expect("Connected to server");
+
+    let _handle = tokio::spawn(async move { w.run(&["default"]).await });
+
+    // wait for processing results
+    let res = rx.recv().await.expect("some calculation result");
+
+    tracing::info!(
+        "Send a job with args `vec![5, 8]`. Receiced result `{}`",
+        res
+    );
+}
--- src/worker/health.rs
+++ src/worker/health.rs
@@ -8,6 +8,8 @@ use std::{
 use tokio::io::{AsyncBufRead, AsyncWrite};
 use tokio::time::sleep as tokio_sleep;
 
+const CHECK_STATE_INTERVAL_MILLIS: u64 = 100;
+const HEARTBEAT_INTERVAL_SECS: u64 = 5;
 impl<S, E> Worker<S, E>
 where
     S: AsyncBufRead + AsyncWrite + Send + Unpin,
@@ -22,6 +24,10 @@ where
     ///   but should _continue_ processing its current job (if any);
     ///
     /// See more details [here](https://github.com/contribsys/faktory/blob/b4a93227a3323ab4b1365b0c37c2fac4f9588cc8/server/workers.go#L13-L49).
+    ///
+    /// Note that this method is not cancellation safe. We are using an interval timer internally, that
+    /// would be reset should we call this method anew. Besides, the `Heartbeat` command is being issued
+    /// with the help of `AsyncWriteExt::write_all` which is not cancellation safe either.
     pub(crate) async fn listen_for_heartbeats(
         &mut self,
         statuses: &[Arc<atomic::AtomicUsize>],
@@ -31,14 +37,14 @@ where
         let mut last = time::Instant::now();
 
         loop {
-            tokio_sleep(time::Duration::from_millis(100)).await;
+            tokio_sleep(time::Duration::from_millis(CHECK_STATE_INTERVAL_MILLIS)).await;
 
             // has a worker failed?
-            if target == STATUS_RUNNING
+            let worker_failure = target == STATUS_RUNNING
                 && statuses
                     .iter()
-                    .any(|s| s.load(atomic::Ordering::SeqCst) == STATUS_TERMINATING)
-            {
+                    .any(|s| s.load(atomic::Ordering::SeqCst) == STATUS_TERMINATING);
+            if worker_failure {
                 // tell all workers to exit
                 // (though chances are they've all failed already)
                 for s in statuses {
@@ -47,7 +53,7 @@ where
                 break Ok(false);
             }
 
-            if last.elapsed().as_secs() < 5 {
+            if last.elapsed().as_secs() < HEARTBEAT_INTERVAL_SECS {
                 // don't sent a heartbeat yet
                 continue;
             }
@@ -55,8 +61,11 @@ where
             match self.c.heartbeat().await {
                 Ok(hb) => {
                     match hb {
-                        HeartbeatStatus::Ok => {}
+                        HeartbeatStatus::Ok => {
+                            tracing::trace!("Faktory server HEARTBEAT status is OK.");
+                        }
                         HeartbeatStatus::Quiet => {
+                            tracing::trace!("Faktory server HEARTBEAT status is QUIET.");
                             // tell the workers to eventually terminate
                             for s in statuses {
                                 s.store(STATUS_QUIET, atomic::Ordering::SeqCst);
@@ -64,6 +73,7 @@ where
                             target = STATUS_QUIET;
                         }
                         HeartbeatStatus::Terminate => {
+                            tracing::trace!("Faktory server HEARTBEAT status is TERMINATE.");
                             // tell the workers to terminate
                             // *and* fail the current job and immediately return
                             for s in statuses {
