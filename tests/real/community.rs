be454655b578e62787fe9a0ee40ee2138662863f
Pavieł Michalkievič <117771945+rustworthy@users.noreply.github.com>
2024-05-20 19:24:58 +0400

    Update tests/real/community.rs
    Co-authored-by: Jon Gjengset <jon@thesquareplanet.com>

--- tests/real/community.rs
+++ tests/real/community.rs
@@ -92,7 +92,7 @@ async fn server_state() {
     // the following two assertions are not super-helpful but
     // there is not much info we can make meaningful assetions on anyhow
     // (like memusage, server description string, version, etc.)
-    assert!(server_state.server.connections >= 2); // at least two clients from the current test
+    assert!(server_state.server.connections >= 2, "{}", server_state.server.connections); // at least two clients from the current test
     assert!(server_state.server.uptime > 0); // if IPC is happenning, this should hold :)
 
     // push 1 job
