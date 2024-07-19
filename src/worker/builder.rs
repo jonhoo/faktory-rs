e0cb357e3c3f7ce0e25f7d5dc2f3af5b4d9aaddf
--show-origin <paul.michelle.prod@gmail.com>
2024-07-19 10:13:57 +0400

    StopDetails::stop_reason -> StopDetails::reason

--- src/worker/builder.rs
+++ src/worker/builder.rs
@@ -147,7 +147,7 @@ impl<E: 'static> WorkerBuilder<E> {
     ///
     /// // learn the stop reason and the number of workers that were still running
     /// let stop_details = jh.await.expect("joined ok").unwrap();
-    /// assert_eq!(stop_details.stop_reason, StopReason::GracefulShutdown);
+    /// assert_eq!(stop_details.reason, StopReason::GracefulShutdown);
     /// let _nrunning = stop_details.nrunning;
     /// # });
     /// ```
